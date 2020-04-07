874e347cf0a84ba1caa35c27fbbec95e8b66e3e4
cydneymikel <cydneymikel@gmail.com>
2020-04-06 20:02:46 -0700

    updated app and index.

--- Week11/metmuseum/met-app/app.js
+++ Week11/metmuseum/met-app/app.js
@@ -13,41 +13,108 @@ const _print = (data) => {
 };
 
 const _filterDepartments = (departments, term) => {
+    const searched = departments.filter((department) => {
+        return department.displayName
+            .toLowerCase()
+            .includes(term.toLowerCase());
+    });
 
+    return searched;
 };
 
 const _displayArtResults = (results) => {
+    const displayArt = results.map((result) => {
+        return {
+            name: `${result.title} - ${result.artist || 'Unknown'}`,
+            value: result.id,
+        };
+    });
 
+    return inquirer.prompt([
+        {
+            type: 'list',
+            name: 'id',
+            message: 'Select an artwork.',
+            choices: displayArt,
+        },
+    ]);
 };
 
-const _displayDepartmentResults = (results) => {
+const _displayDepartments = (results) => {
+    const displayDepts = results.map((result) => {
+        return {
+            name: result.displayName,
+            value: result.departmentId,
+        };
+    });
 
+    return inquirer.prompt([
+        {
+            type: 'list',
+            name: 'id',
+            message: 'Select an department.',
+            choices: displayDepts,
+        },
+    ]);
 };
 
 const _promptUser = (question) => {
-
+    return inquirer.prompt([
+        {
+            type: 'confirm',
+            name: 'answer',
+            message: question,
+        },
+    ]);
 };
 
-const _userInput = (type) => {
-
+const _userInput = (question) => {
+    return inquirer.prompt([
+        {
+            type: 'input',
+            name: 'answer',
+            message: question,
+        },
+    ]);
 };
 
-const searchArt = async (term, department) => {
-    const response = await met.search(term, department);
+const searchArt = async (term, department = null) => {
+    const format = true;
+    const limit = 20;
 
+    const response = await met.search(term, department, format, limit);
+    const choice = await _displayArtResults(response);
+    const artwork = await met.fetch(choice.id);
+    _print(artwork);
 };
 
 const searchDepartment = async (term = '') => {
     const response = await met.departments();
+    const filtered = _filterDepartments(response.departments, term);
+    const choice = await _displayDepartments(filtered);
 
+    console.log(choice);
 };
 
 const advanceSearch = async () => {
+    // prompt the user to search by department by filling _promptUser()
 
-}
+    // if the user answers true
+    // prompt the user for a department term by using _userInput()
+    // now the the search term call searchDepartment(term)
+
+    // now prompt the user for an artwork term by using _userInput()
+
+    // mow that you have both a department id and an artwork term
+    // now call searchArt(term, department)
+
+    // else
+    // now prompt the user for an artwork term by using _userInput()
+    // searchArt(term)
+};
 
 module.exports = {
     searchArt,
     searchDepartment,
-    advanceSearch
+    advanceSearch,
 };
--- Week11/metmuseum/met-module/index.js
+++ Week11/metmuseum/met-module/index.js
@@ -1,12 +1,48 @@
 const config = require('./config');
 const superagent = require('superagent');
 
-const _formatDisplay = (response, limit) => {};
+const _formatDisplay = (response, limit) => {
+    const { objectIDs } = response;
+    const limited = objectIDs.splice(0, limit);
 
-const search = async (term) => {
+    // loop of the results and fetch each result by id
+    // const formattedForLoop = []
+    // for (let i = 0; i < limited.length; i++) {
+    //     const artwork = await fetch(id)
+
+    //     formattedForLoop.push({
+    //         title: artwork.title,
+    //         artist: artwork.artistDisplayName,
+    //         id: objectID
+    //     })
+    // };
+
+    const formatted = Promise.all(limited.map(async id => {
+        const artwork = await fetch(id);
+
+        return {
+            title: artwork.title,
+            artist: artwork.artistDisplayName,
+            id: artwork.objectID
+        }
+    }));
+
+    return formatted;
+};
+
+const search = async (term, department, format = true, limit = 20) => {
+    // if there is a department
+    // then apply the query &departmentId=${department}
+
     try {
         const response = await superagent.get(`${config.url}/search?q=${term}`);
-        return response.body;
+
+        if (format) {
+            return _formatDisplay(response.body, limit);
+        } else {
+            return response.body;
+        }
+
     } catch (error) {
         return error;
     }
@@ -21,7 +57,7 @@ const fetch = async (id) => {
     }
 };
 
-const departments = async (id) => {
+const departments = async () => {
     try {
         const response = await superagent.get(`${config.url}/departments`);
         return response.body;
