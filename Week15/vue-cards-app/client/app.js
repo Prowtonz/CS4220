dae31aaecbf11eb4e3181af810ba4bd17931b9b9
cydneymikel <cydneymikel@gmail.com>
2020-05-04 20:28:45 -0700

    updated component demo.

--- Week15/vue-cards-app/client/app.js
+++ Week15/vue-cards-app/client/app.js
@@ -1,3 +1,24 @@
+// declaring a component and storing it in the variable gameTrackerComponent
+// This component is using the template and props options
+const gameTrackerComponent = {
+    template: ` <div>
+                    <div class="col" v-for="game in gameTracker">
+                    <ul>
+                        <li>
+                            <small>
+                                Deck Used: {{ game.deck_id }}
+                                <br/>
+                                Remaining Cards: {{ game.remaining }}
+                                <br/>
+                                @ {{ game.time }}
+                            </small>
+                        </li>
+                    </ul>
+                    </div>
+                </div>`,
+    props: ['gameTracker'],
+};
+
 const cards = new Vue({
     el: '#cards',
     data: {
@@ -34,13 +55,31 @@ const cards = new Vue({
             this.deck = response.data;
             this.isPlaying = false;
         },
+        trackGame: function () {
+            // create a new date and time string formatted to local time
+            const now = new Date().toLocaleString('en-US');
+
+            // push game data that we want to add to the history
+            this.gameTracker.push({
+                deck_id: this.deck.deck_id,
+                remaining: this.deck.remaining,
+                time: now,
+            });
+        },
         clear: function () {
+            // the trackGame() function is called from the clear() function
+            // it tracks some information before we reset the game data
             this.trackGame();
 
             this.deck = {};
             this.numberOfCards = '';
             this.shuffle = '';
             this.selectedCards = [];
-        }
-    }
+        },
+    },
+    components: {
+        // the key (left side) is the the components name
+        // the value (right side) is the component itself as declared at the top
+        'tracker-component': gameTrackerComponent,
+    },
 });
